363ad87356b4bb599d6ccc123a51f38f82424705
Paloma Oliveira <80291333+Paah-76@users.noreply.github.com>
2024-08-15 08:27:15 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Atualizar o sistema
+yum update -y
+
+# Instalar o Apache
+yum install -y httpd
+
+# Iniciar o Apache e configurá-lo para iniciar automaticamente no boot
+systemctl start httpd
+systemctl enable httpd
+
+# Criar um arquivo HTML vazio
+touch /var/www/html/index.html
+
+# Garantir que as permissões estejam corretas
+chown apache:apache /var/www/html/index.html
